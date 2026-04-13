8e030c55fd26f1276d5cb2eb2ada328c69c320f5
pheebemi <lemuelemmanuel29@gmail.com>
2026-04-13 17:25:33 +0100

    rick fix

--- lms_backend/settings.py
+++ lms_backend/settings.py
@@ -19,8 +19,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'api.sittechnologies.com',
-    '.sittechnologies.com',
+    'api.sittechnologies.ng',
+    '.sittechnologies.ng',
 ]
 
 # Application definition
